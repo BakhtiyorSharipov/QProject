576bd2bd859d0dbee53254581fa96279409bd9b5
BakhtiyorSharipov <bsharipov731@gmail.com>
2026-02-23 18:51:42 +0500

    Added new migration

---
+++ QService/QInfrastructure/Persistence/DataBase/Migrations/20260223135111_AddedNewProperties.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QInfrastructure.Persistence.DataBase.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedNewProperties : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "BranchId",
+                table: "Users",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "CompanyId",
+                table: "Users",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "BranchId",
+                table: "Queues",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "CompanyId",
+                table: "Queues",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_BranchId",
+                table: "Users",
+                column: "BranchId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_CompanyId",
+                table: "Users",
+                column: "CompanyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Queues_BranchId",
+                table: "Queues",
+                column: "BranchId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Queues_CompanyId",
+                table: "Queues",
+                column: "CompanyId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Users_BranchId",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_CompanyId",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Queues_BranchId",
+                table: "Queues");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Queues_CompanyId",
+                table: "Queues");
+
+            migrationBuilder.DropColumn(
+                name: "BranchId",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "CompanyId",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "BranchId",
+                table: "Queues");
+
+            migrationBuilder.DropColumn(
+                name: "CompanyId",
+                table: "Queues");
+        }
+    }
+}
