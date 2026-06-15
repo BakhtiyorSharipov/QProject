1b4a6f8e88805088c27eea3a06b615543152726f
BakhtiyorSharipov <bsharipov731@gmail.com>
2026-06-15 17:13:40 +0500

    Implemented UserService Entities and Migration

--- QUserService/QUserService.API/Program.cs
+++ QUserService/QUserService.API/Program.cs
@@ -1,5 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using QUserService.Infrastructure.Persistence.Database;
+
 var builder = WebApplication.CreateBuilder(args);
 
+
+builder.Services.AddDbContext<UserServiceDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
